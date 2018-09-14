5b14899014a4ec950278a429da5d40d233909d55
Baron <Baron@172.18.36.218>
2018-09-14 09:45:10 +0200

    Le JAR va t il marcher

--- src/Interface/MainFrame.java
+++ src/Interface/MainFrame.java
@@ -1,6 +1,5 @@
 package Interface;
 
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -36,7 +35,7 @@ public class MainFrame extends JFrame implements ActionListener{
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					final MainFrame frame = new MainFrame();
